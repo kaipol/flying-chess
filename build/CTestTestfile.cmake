8234ce431eae2c16d12236d6f7e4f1bbbf4dc313
kaipol <1335791724@qq.com>
2022-12-25 17:28:16 +0800

    deleted cmakelist

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Project/flychess
-# Build directory: E:/Project/flychess/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
